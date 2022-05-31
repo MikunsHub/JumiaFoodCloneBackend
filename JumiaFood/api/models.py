c7edc2ed466630b9f48c6413eac0ac9296bba534
MikunsHub <63354714+MikunsHub@users.noreply.github.com>
2022-05-31 10:27:32 +0100

    deleted my test models

--- JumiaFood/api/models.py
+++ JumiaFood/api/models.py
@@ -2,11 +2,4 @@ from django.db import models
 
 # Create your models here.
 
-class Order(models.Model):
-    payment_type = models.CharField(max_length=80)
-    timestamp = models.DateTimeField()
 
-
-class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
